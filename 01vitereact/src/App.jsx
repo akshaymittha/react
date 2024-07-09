75170254b96f9ddc624484c2af29b97536192196
akshay_mittha <akshaymittha7@gmail.com>
2024-07-09 11:56:03 +0530

    01-vite-react

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./chai"
+
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>Learn React | Akshay </h1>
+    </>
+  )
+}
+
+export default App
+
+
+// <></>  -> fragment 
+// ctr + shift + p  -> Reload window
