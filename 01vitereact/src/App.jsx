ebc25a7f657ff551dc9b1e14071d39f157323409
akshay_mittha <akshaymittha7@gmail.com>
2024-07-09 14:26:32 +0530

    Custom React

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,12 @@
 import Chai from "./chai"
 
 function App() {
+  const username = "chai aur code"
   return (
     <>
       <Chai />
       <h1>Learn React | Akshay </h1>
+      <h1>chai aur react, {username}</h1>
     </>
   )
 }
